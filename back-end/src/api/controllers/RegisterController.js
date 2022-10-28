a102b7a153df5e08fac7756e5a5963bf042d6a50
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-10-28 20:06:18 -0300

    fix: refatora back end de Register para retornar name, email, password, app funcionando, cadastrando usuario e conversando com o front, bugando avalidor requisitos 9 e 10

--- back-end/src/api/controllers/RegisterController.js
+++ back-end/src/api/controllers/RegisterController.js
@@ -5,12 +5,15 @@ const RegisterController = {
   Register: async (req, res) => {
     const { body } = req;
     const result = await Register.Register(body);
-    console.log(body);
+    console.log(result);
     if (result === null) { // se retornar nulo (se usuario ja existir)
       // return res.status(409).json({ message: 'Usuario ja cadastrado' });
       throw new Error('409|Usuario ja cadastrado');
     }
-    return res.status(201).json({ result });
+    const userName = result.name;
+    const userEmail = result.email;
+    const userPass = result.password;
+    return res.status(201).json({ name: userName, email: userEmail, password: userPass });
   },
 };
 
