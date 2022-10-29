4e755080ddfbd732e1130895da1c47bcc3a58ead
kaioruan <kaio.ruan@souunit.com.br>
2022-10-29 14:38:59 -0300

    feat: create router get /customer/products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const RouterLogin = require('./router/RouterLogin');
+const RouterProducts = require('./router/RouterProduct');
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/', RouterLogin);
+app.use('/', RouterProducts);
+app.use('/images', express.static('public'));
 
 app.use((err, _req, res, _next) => {
   // console.error('middleware error',err);
