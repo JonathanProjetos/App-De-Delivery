0866d6e9b32538417dbeced13078ee8240815d0a
kaioruan <kaio.ruan@souunit.com.br>
2022-10-29 16:26:47 -0300

    feat: create route validate login

--- back-end/src/api/router/RouterLogin.js
+++ back-end/src/api/router/RouterLogin.js
@@ -4,5 +4,5 @@ const RouterLogin = express.Router();
 const LoginController = require('../controllers/LoginController');
 
 RouterLogin.post('/login', LoginController.Login);
-
+RouterLogin.post('/login/validate', LoginController.loginValidate);
 module.exports = RouterLogin;
