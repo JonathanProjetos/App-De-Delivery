d5012a1aaf24dc39b92efcf8d98391d48820010c
kaioruan <kaio.ruan@souunit.com.br>
2022-10-31 19:47:47 -0300

    feat: create state total for childrens acess

--- front-end/src/pages/Produtos.js
+++ front-end/src/pages/Produtos.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { requestData } from '../services/request';
 import ProductCard from '../components/ProductCard';
 
 function Produtos() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [updateTotal, setUpdateTotal] = useState(false);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     const loginValidate = async () => {
@@ -28,14 +30,33 @@ function Produtos() {
     loginValidate();
   }, []);
 
+  useEffect(() => {
+    if (updateTotal) {
+      const totalCart = localStorage.getItem('total');
+      console.log(totalCart);
+      setTotal(totalCart);
+    }
+    setUpdateTotal(false);
+  }, [updateTotal]);
+
   return (
     <section>
+      <button
+        type="button"
+        onClick={ () => navigate('/customer/checkout') }
+        datatest-id="customer_products__checkout-bottom-value"
+      >
+        VER CARRINHO :
+        R$
+        {' '}
+        {total}
+      </button>
       <div>
         { products.map((product) => (
           <ProductCard
             key={ product.id }
             product={ product }
-            // setProducts={ setProducts }
+            setUpdateTotal={ setUpdateTotal }
           />
         ))}
       </div>
