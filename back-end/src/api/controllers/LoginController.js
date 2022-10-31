67d08ddfffe85daccec06793c0c192a159c5741b
kaioruan <kaio.ruan@souunit.com.br>
2022-10-31 19:48:24 -0300

    fix: resolves error to validate login

--- back-end/src/api/controllers/LoginController.js
+++ back-end/src/api/controllers/LoginController.js
@@ -10,11 +10,13 @@ const LoginController = {
     return res.status(200).json({ token: tokenData, role });
   },
   loginValidate: async (req, res) => {
-    const { authorization } = req.headers;
-    if (!authorization) return res.status(401).json({ message: 'missing auth token' });
+    const { email } = req.user;
     try {
-      const result = await Login.loginValidate(authorization);
-      return res.status(200).json(result);
+      const result = await Login.loginValidate(email);
+      if (result) {
+        return res.status(200).json({ message: 'ok' });
+      } 
+        throw new Error('404|Pessoa não cadastrada');
     } catch (err) {
       return res.status(401).json({ message: err.message });
     }
