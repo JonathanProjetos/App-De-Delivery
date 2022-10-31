3a6b0e9e0043bb2c6cc611790c9953b08f3eefaf
kaioruan <kaio.ruan@souunit.com.br>
2022-10-31 18:06:45 -0300

    feat: create component and refactore to render cardProduct

--- front-end/src/pages/Produtos.js
+++ front-end/src/pages/Produtos.js
@@ -1,53 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { requestData, requestLogin } from '../services/request';
+// import { useNavigate } from 'react-router-dom';
+import { requestData } from '../services/request';
+import ProductCard from '../components/ProductCard';
 
 function Produtos() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const loginValidate = async () => {
       try {
-        const token = localStorage.getItem('token');
-        const validate = await requestLogin('/login/validate', { token });
-        if (!validate) {
-          navigate('/login');
-        }
+        // const token = localStorage.getItem('token');
+        // const validate = await requestLogin('/login/validate', { token });
+        // if (!validate) {
+        //   navigate('/login');
+        // }
         const data = await requestData('/customer/products');
+        // data.forEach((product) => {
+        //   product.quantity = 0;
+        // });
         setProducts(data);
       } catch (error) {
-        navigate('/login');
+        // navigate('/login');
         console.log(error);
       }
     };
     loginValidate();
-  }, [navigate]);
+  }, []);
+
   return (
     <section>
       <div>
-        {products.map((product) => (
-          <div key={ product.id }>
-            <p
-              datatest-id={ `customer_products__element-card-title-${product.id}` }
-            >
-              {product.name}
-            </p>
-            <p
-              datatest-id={ `customer_products__element-card-price-${product.id}` }
-            >
-              {product.price}
-            </p>
-            <img
-              datatest-id={ `customer_products__img-card-bg-image-${product.id}` }
-              src={ product.url_image }
-              alt={ product.name }
-            />
-            <p>
-              {!product.quantity ? 0 : product.quantity}
-
-            </p>
-          </div>
+        { products.map((product) => (
+          <ProductCard
+            key={ product.id }
+            product={ product }
+            // setProducts={ setProducts }
+          />
         ))}
       </div>
     </section>
