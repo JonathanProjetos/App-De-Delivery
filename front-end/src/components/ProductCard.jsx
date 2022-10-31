763dd7a76dba2f2b1f61a62dfe2175b71883bcc5
kaioruan <kaio.ruan@souunit.com.br>
2022-10-31 19:47:24 -0300

    feat: implement logic to insert price in total cart

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,48 +1,58 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from '../context/Context';
 
-function ProductCard({ product }) {
+function ProductCard({ product, setUpdateTotal }) {
   const { cart, setCart } = useContext(Context);
   const [productCard, setProductCard] = useState(product);
-  const total = JSON.parse(localStorage.getItem('total'));
+  useEffect(() => {
+    const cartStorage = localStorage.getItem('cart');
+    if (cartStorage) {
+      const totalValue = cart.reduce((
+        acc,
+        { price, quantity },
+      ) => acc + (price * quantity), 0).toFixed(2);
+      localStorage.setItem('total', totalValue);
+    }
+  }, [cart]);
 
-  const incrementar = async () => {
+  const incrementar = () => {
     try {
-      const { price, quantity } = productCard;
-      const quantityUpdate = productCard.quantity === 0 ? 1 : productCard.quantity + 1;
-      // setQuantity((prevQuantity) => prevQuantity + 1);
+      const { quantity } = productCard;
+      const quantityUpdate = quantity === 0 ? 1 : quantity + 1;
       setProductCard((prev) => ({
         ...prev,
         quantity: quantityUpdate,
       }));
       const oldCart = cart.filter((item) => item.id !== productCard.id);
-      setCart([...oldCart, { ...productCard }]);
-      localStorage.setItem('total', JSON.stringify(total + (quantity * Number(price))));
-      localStorage.setItem('cart', JSON.stringify([...oldCart, { ...productCard }]));
-      const test = JSON.parse(localStorage.getItem('cart'));
-      // console.log(test);
-      console.log(test);
+      setCart([...oldCart, { ...productCard, quantity: quantityUpdate }]);
+      localStorage.setItem('cart', JSON.stringify(
+        [...oldCart, { ...productCard, quantity: quantityUpdate }],
+      ));
+      setUpdateTotal(true);
     } catch (error) {
       console.log(error);
     }
   };
 
   const decrementar = () => {
-    const quantityUpdate = productCard.quantity === 1 ? 0 : productCard.quantity - 1;
+    const { quantity } = productCard;
+    const quantityUpdate = quantity === 1 ? 0 : quantity - 1;
     setProductCard((prev) => ({
       ...prev,
       quantity: quantityUpdate,
     }));
-    const { price, quantity } = productCard;
+    setUpdateTotal(true);
     const oldCart = cart.filter((item) => item.id !== productCard.id);
-    setCart([...oldCart, { ...productCard }]);
-    localStorage.setItem('total', JSON.stringify(total - (quantity * Number(price))));
-    localStorage.setItem('cart', JSON.stringify([...oldCart, { ...productCard }]));
-    if (productCard.quantity === 0) {
-      const newCart = cart.filter((item) => item.id !== productCard.id);
-      setCart(newCart);
-      localStorage.setItem('cart', JSON.stringify(newCart));
+    if (quantityUpdate === 0) {
+      setCart(oldCart);
+      localStorage.setItem('cart', JSON.stringify(oldCart));
+    } else {
+      setCart([...oldCart, { ...productCard, quantity: quantityUpdate }]);
+      localStorage.setItem(
+        'cart',
+        JSON.stringify([...oldCart, { ...productCard, quantity: quantityUpdate }]),
+      );
     }
   };
 
@@ -108,6 +118,7 @@ ProductCard.propTypes = {
     url_image: PropTypes.string,
     quantity: PropTypes.number,
   }).isRequired,
+  setUpdateTotal: PropTypes.func.isRequired,
 };
 
 export default ProductCard;
