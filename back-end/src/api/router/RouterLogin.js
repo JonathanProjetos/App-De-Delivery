2b0357f4b94ed38f7f42a9f4f38e92bb9311657a
kaioruan <kaio.ruan@souunit.com.br>
2022-10-31 19:49:23 -0300

    feat: import validate token to route /login/validate

--- back-end/src/api/router/RouterLogin.js
+++ back-end/src/api/router/RouterLogin.js
@@ -1,8 +1,11 @@
+// import validateToken from '../middleware/ValidateToken';
+
 const express = require('express');
 
 const RouterLogin = express.Router();
+const ValidateToken = require('../middleware/ValidateToken');
 const LoginController = require('../controllers/LoginController');
 
 RouterLogin.post('/login', LoginController.Login);
-RouterLogin.post('/login/validate', LoginController.loginValidate);
+RouterLogin.post('/login/validate', ValidateToken.loginToken, LoginController.loginValidate);
 module.exports = RouterLogin;
