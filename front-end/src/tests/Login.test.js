0cf56e021f363758829b4b341aacc6b6b7e13fde
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-11-01 16:29:39 -0300

    fix: tentando consertar import do axios no arquivo de test do front da pagina Login

--- front-end/src/tests/Login.test.js
+++ front-end/src/tests/Login.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import * as axios from "axios";
 import { screen, cleanup, waitForElement, render } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 import axiosMock from './testsMocks/axios';
 import userMock from './testsMocks/user';
 import Login from '../pages/Login';
 
-jest.mock()
+jest.mock(axios);
 
 describe('Testando pagina Login', () => {
   axiosMock.get.mockResolvedValueOnce(userMock.loginUser);
