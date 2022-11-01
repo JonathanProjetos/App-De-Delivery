ad721e95500c82f3cf37fcf65b992a1287078245
kaioruan <kaio.ruan@souunit.com.br>
2022-11-01 16:32:11 -0300

    feat: create route customer/orders to get order by user

--- back-end/src/api/router/RouterProduct.js
+++ back-end/src/api/router/RouterProduct.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const RouterProducts = express.Router();
 const ProductsController = require('../controllers/ProductsController');
+const SalesController = require('../controllers/SalesController');
+const ValidateToken = require('../middleware/ValidateToken');
 
 RouterProducts.get('/customer/products', ProductsController.getProducts);
+RouterProducts.get('/customer/orders', ValidateToken.loginToken, SalesController.getSalesById);
 
 module.exports = RouterProducts;
