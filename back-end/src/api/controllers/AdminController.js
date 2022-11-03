b619018011544575acc2287a9ab06e14df4bccfc
kaioruan <kaio.ruan@souunit.com.br>
2022-11-03 18:49:20 -0300

    fix: resolves error in delete / create user

--- back-end/src/api/controllers/AdminController.js
+++ back-end/src/api/controllers/AdminController.js
@@ -7,14 +7,14 @@ const AdminController = {
     return res.status(200).json(result);
   },
   deleteUser: async (req, res) => {
-    const { id } = req.body;
+    const { id } = req.params;
     const result = await Admin.deleteUser(id);
     return res.status(200).json(result);
   },
   createUser: async (req, res) => {
     const { body } = req;
     const result = await Admin.createUser(body);
-    return res.status(200).json(result);
+    return res.status(201).json(result);
   },
 };
 
