5e3ceb0c4b92093c06f21061bfbc1c883ceec0b0
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-11-03 15:06:51 -0300

    fix: tentativa de desbuggar testes do frontend, bugando no pathname, retorna pathname undefined

--- front-end/src/tests/Login.test.js
+++ front-end/src/tests/Login.test.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import * as axios from "axios";
+import axios from 'axios';
 import { screen, cleanup, waitForElement, render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-import axiosMock from './testsMocks/axios';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+// import axiosMock from './testsMocks/axios';
 import userMock from './testsMocks/user';
 import Login from '../pages/Login';
+import App from '../App';
 
-jest.mock(axios);
+jest.mock('axios');
 
-describe('Testando pagina Login', () => {
-  axiosMock.get.mockResolvedValueOnce(userMock.loginUser);
-  afterEach(cleanup);
-
-  it('testa o input email', () => {
+describe('Pagina Login', () => {
+  it('existe um input email, um input password e o botao de login', () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>,
+    );
+    const emailInput = screen.getByTestId('common_login__input-email');
+    const passInput = screen.getByTestId('common_login__input-email');
+    const enterButton = screen.getByTestId('common_login__button-login');
+    expect(passInput && emailInput && enterButton).toBeInTheDocument();
+  });
+  it('login com usuario valido redireciona para pagina /customer/products', () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>,
+    );
+    const { history } = renderWithRouter(<App />);
+    const enterButton = screen.getByTestId('common_login__button-login');
+    expect(enterButton).toBeInTheDocument();
+    axios.post.mockResolvedValueOnce(userMock.loginUser);
+    userEvent.click(enterButton);
+    expect(history.location.pathname).toBe('/customer/products');
   });
 });
---
+++ front-end/src/tests/renderWithRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+
+function renderWithRouter(component) {
+  const customHistory = createMemoryHistory();
+
+  const returnFromRender = render(
+    <Router history={ customHistory }>
+      {component}
+    </Router>,
+  );
+
+  return { history: customHistory, ...returnFromRender };
+}
+
+export default renderWithRouter;
