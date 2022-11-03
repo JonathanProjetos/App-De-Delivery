8edaed44ac9aa03a440467497ac2d72d3d743fd0
kaioruan <kaio.ruan@souunit.com.br>
2022-11-03 15:33:30 -0300

    feat: create AdminController create, getAllUsers, delete Users

---
+++ back-end/src/api/controllers/AdminController.js
@@ -0,0 +1,21 @@
+const Admin = require('../services/AdminService');
+
+const AdminController = {
+
+  getAllUsers: async (req, res) => {
+    const result = await Admin.getAllUsers();
+    return res.status(200).json(result);
+  },
+  deleteUser: async (req, res) => {
+    const { id } = req.body;
+    const result = await Admin.deleteUser(id);
+    return res.status(200).json(result);
+  },
+  createUser: async (req, res) => {
+    const { body } = req;
+    const result = await Admin.createUser(body);
+    return res.status(200).json(result);
+  },
+};
+
+module.exports = AdminController;
