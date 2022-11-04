88734dcc8ef20f42ec10bb59ce56e1b7d6c674a0
Jonathan Santos <jonathan01347@gmail.com>
2022-11-04 17:03:57 -0300

    fikx(validLogin) Alteração na função de validação do login

--- front-end/src/pages/MeusPedidos.js
+++ front-end/src/pages/MeusPedidos.js
@@ -2,22 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Orders from '../components/Orders';
 import Header from '../components/Header';
-import { validLogin } from '../services/request';
+import { validLogin, setToken } from '../services/request';
 
 function MeusPedidos() {
   const [user, setUser] = useState(undefined);
   const navigate = useNavigate();
 
   useEffect(() => {
-    (async () => {
-      const loggedUser = JSON.parse(localStorage?.getItem('user')) || navigate('/login');
-      const validToken = await validLogin('/login/validate');
-      if (validToken) {
-        setUser(loggedUser);
-      } else {
-        navigate('/login');
+    // validação para token ao acessar a page
+    const getToken = JSON.parse(localStorage.getItem('user'));
+    const { token } = getToken;
+    setUser(getToken);
+    const requestValid = async () => {
+      try {
+        setToken(token);
+        const validToken = await validLogin('/login/validate');
+        if (!validToken) {
+          navigate('/login');
+        }
+      } catch (error) {
+        console.log(error);
       }
-    })();
+    };
+    requestValid();
   }, [navigate]);
 
   return (
