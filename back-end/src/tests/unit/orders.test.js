d1fcd9f4e4b5d5d9b7c1b8a461af451ab3789c82
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-11-07 17:34:42 -0300

    feat: testes da rota /customer/orders, arquivo orders.test.js, funcionando

--- back-end/src/tests/unit/orders.test.js
+++ back-end/src/tests/unit/orders.test.js
@@ -4,21 +4,31 @@ const chaiHttp  = require('chai-http');
 const sinon = require("sinon");
 const app = require('../../api/app');
 
-const logintService = require('../../api/services/LoginService');
-const orderMock = require("../mocks/order.mocks");
+const orderMock = require("../mocks/order.mock");
+const { loginMock } = require("../mocks/login.mocks");
 const { expect } = chai;
+const jwtCheckLogin = require('../../api/middleware/Token');
 chai.use(chaiHttp);
 
-describe("Testes da rota /customer/orders", () => {
-
+describe("Testes da rota /customer/orders", () => {  
   afterEach(()=>{
     sinon.restore();
   });
-
+  
   describe("GET SUCCESS", () => {
-    it("get feito com sucesso", async function () {
-      
-    });
-    
+    it("get feito com sucesso quando token for valido", async function () {
+      const token = await jwtCheckLogin.generateToken(loginMock.email);
+      const response = await chai.request(app)
+      .get('/customer/orders')
+      .set('authorization', token);
+      expect(response.status).to.equal(200);
+    });    
+  });  
+  describe("GET FAILED", () => {
+    it("get falha quando token for invalido", async function () {
+      const response = await chai.request(app)
+      .get('/customer/orders')
+      expect(response.status).to.equal(401);
+    });    
   });  
 });
