5b53a5c63e098aede8a71835621ba5cd04066bb4
Jonathan Santos <jonathan01347@gmail.com>
2022-11-07 20:14:42 -0300

    fix:(query) Correção na tabela e na query para busca de produtos.

--- back-end/src/api/services/SaleDatailService.js
+++ back-end/src/api/services/SaleDatailService.js
@@ -2,19 +2,28 @@ const { sale, user, product } = require('../../database/models');
 
 const SaleDetailService = {
 
-  getSalesById: async (id) => {
-    const result = await sale.findAll({
-      where: { id }, 
-        include: [{
-          model: user,
-          as: 'user',
-         }, {
-          model: product,
-          as: 'products',
+  getSalesById: async (id, email) => {
+    const result = await user.findAll({
+      where: { email },
+      include: [{
+          model: sale,
+          as: 'sale',
+          where: { id },
+          include: {
+              model: product,
+              as: 'products',
+          },  
         }],
     });
-
     return result;
+  },
+
+  updateStatus: async (id, status) => {
+    const saleId = await user.findOne({ where: { id } });
+    if (!saleId) throw new Error('404|Venda não encontrada');
+
+    await sale.update({ status },
+      { where: { id } });
   },
 };
 
