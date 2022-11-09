5c0fd7c93430b3da2c435fa5ae9c4f5e4f27ce0f
Jonathan Santos <jonathan01347@gmail.com>
2022-11-09 09:00:16 -0300

    feat(MeusPedidosSeller)Página para renderização de pedidos no end-point viculado a seller

---
+++ front-end/src/pages/MeusPedidosSeller.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import OrdersSeller from '../components/OrderSeller';
+import HeaderSeller from '../components/HeaderSeller';
+import { validLogin, setToken } from '../services/request';
+
+function MeusPedidosSeller() {
+  const [roleUser, setUserRole] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // validação para token ao acessar a page
+    const getToken = JSON.parse(localStorage.getItem('user'));
+    const { token, role } = getToken;
+    setUserRole(role);
+    const requestValid = async () => {
+      try {
+        setToken(token);
+        const validToken = await validLogin('/login/validate');
+        if (!validToken) {
+          localStorage.setItem('user', '');
+          navigate('/login');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    requestValid();
+  }, [navigate]);
+
+  return (
+    <div>
+      { roleUser ? (
+        <div>
+          <HeaderSeller />
+          <OrdersSeller role={ roleUser } />
+        </div>
+      ) : <p>Carregando...</p> }
+    </div>
+  );
+}
+
+export default MeusPedidosSeller;
