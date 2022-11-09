9c751ee4af664865ca834151ddea3598a508e085
Jonathan Santos <jonathan01347@gmail.com>
2022-11-09 08:40:13 -0300

    fix(type.jsx) Alteração dos arquivos para jsx

--- front-end/src/pages/DetalhesDePedidoSeller.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DetalhesDePedidoSeller() {
-  return (
-    <div>DetalhesDePedidoSeller</div>
-  );
-}
-
-export default DetalhesDePedidoSeller;
--- front-end/src/pages/MeusPedidosSeller.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function MeusPedidosSeller() {
-  return (
-    <div>MeusPedidosSeller</div>
-  );
-}
-
-export default MeusPedidosSeller;
