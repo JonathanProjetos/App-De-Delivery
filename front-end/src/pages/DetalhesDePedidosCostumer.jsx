9b97d9af6081cc4deff6cafbb19cd65c82a89e6d
Jonathan Santos <jonathan01347@gmail.com>
2022-11-09 08:57:07 -0300

    feat(DetalhesDePedidoCustomer) Adicionando page para renderização de detalhes do pedido.

---
+++ front-end/src/pages/DetalhesDePedidosCostumer.jsx
@@ -0,0 +1,196 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { requestData, setToken, validLogin, updateStatus } from '../services/request';
+import Header from '../components/Header';
+
+function DetalheDePedidoCostumer() {
+  const navigate = useNavigate();
+
+  const [dataPedidoDetail, setDataPedidoDetails] = useState('');
+  const [dateSeller, setDetaSeller] = useState('');
+  const [sellerName, setSellerName] = useState('');
+  const [total, setTotal] = useState('');
+  const [dataProducts, setDataproducts] = useState('');
+
+  useEffect(() => {
+    // validação para token ao acessar a page
+    const getToken = JSON.parse(localStorage.getItem('user'));
+    const { token } = getToken;
+    const requestValid = async () => {
+      try {
+        setToken(token);
+        const validToken = await validLogin('/login/validate');
+        if (!validToken) {
+          localStorage.setItem('user', '');
+          navigate('/login');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    requestValid();
+  }, [navigate]);
+
+  useEffect(() => {
+    const getUrl = document.URL;
+    const urlArray = getUrl.split('/');
+    const lastSegment = urlArray[urlArray.length - 1];
+
+    const requestSaleData = async () => {
+      try {
+        const data = await requestData(`/customer/orders/${lastSegment}`);
+        const seller = Object.values(data[0].sale);
+        setDataPedidoDetails(seller);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    requestSaleData();
+  }, [navigate]);
+
+  useEffect(() => {
+    const data = Object.values(dataPedidoDetail);
+    if (data) {
+      data.forEach((i) => {
+        const getSeller = JSON.parse(localStorage.getItem('seller'));
+        const findSellerName = getSeller.find((s) => s.id === i.sellerId);
+        setSellerName(findSellerName.name);
+        const convertDateSeller = new Date(i.saleDate).toLocaleDateString('pt-BR');
+        setDetaSeller(convertDateSeller);
+        setDataproducts(i.products);
+      });
+    }
+  }, [dataPedidoDetail]);
+
+  useEffect(() => {
+    const sumUnitPrice = dataProducts && dataProducts.map((i) => {
+      let totalSum = i.totalProductUnit;
+      totalSum = (i.price * i.saleProduct.quantity);
+      return totalSum.toFixed(2);
+    });
+    setTotal(sumUnitPrice);
+    setDataproducts(dataProducts);
+  }, [dataProducts]);
+
+  const handleUpdateStatus = async () => {
+    const getUrl = document.URL;
+    const urlArray = getUrl.split('/');
+    const lastSegment = urlArray[urlArray.length - 1];
+    try {
+      const objectStatus = {
+        status: 'Entregue',
+        id: dataPedidoDetail && dataPedidoDetail[0].id,
+      };
+      await updateStatus('/customer/orderStatus', { ...objectStatus });
+      const data = await requestData(`/customer/orders/${lastSegment}`);
+      const seller = Object.values(data[0].sale);
+      setDataPedidoDetails(seller);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const titulos = [
+    'Item',
+    'Descrição',
+    'Quantidade',
+    'Valor Unitário',
+    'Sub-total',
+  ];
+
+  return (
+    <div>
+      <Header />
+      <h1>Detalhe do Pedido</h1>
+      <div>
+        <p
+          data-testid="customer_order_details__element-order-details-label-order-id"
+        >
+          {`PEDIDO 000${dataPedidoDetail && dataPedidoDetail[0].id}`}
+
+        </p>
+        <p data-testid="customer_order_details__element-order-details-label-seller-name">
+          {`P.Vend: ${sellerName}`}
+        </p>
+        <p data-testid="customer_order_details__element-order-details-label-order-date">
+          {dateSeller}
+        </p>
+        <p
+          data-testid={ 'customer_order_details__element'
+          + '-order-details-label-delivery-status' }
+        >
+          {dataPedidoDetail && dataPedidoDetail[0].status}
+        </p>
+        <button
+          data-testid="customer_order_details__button-delivery-check"
+          type="button"
+          onClick={ handleUpdateStatus }
+          disabled={ dataPedidoDetail && (
+            dataPedidoDetail[0].status !== 'Em Trânsito') }
+        >
+          MARCAR COMO ENTREGUE
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            {titulos.map((titulo) => (
+              <th key={ titulo }>{titulo}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            dataProducts && dataProducts.map((data, index) => (
+              <tr key={ index }>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-item-number-${index}`
+                  }
+                >
+                  {(index + 1)}
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-name-${index}`
+                  }
+                >
+                  {data.name}
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-quantity-${index}`
+                  }
+                >
+                  {data.saleProduct.quantity}
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-unit-price-${index}`
+                  }
+                >
+                  {`R$ ${data.price.replace('.', ',')}`}
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-sub-total-${index}`
+                  }
+                >
+                  {`R$ ${total && total[index].toString().replace('.', ',')}`}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <p data-testid="customer_order_details__element-order-total-price">
+        {`Total: R$ ${dataPedidoDetail && dataPedidoDetail[0]
+          .totalPrice.toString().replace('.', ',')}`}
+
+      </p>
+    </div>
+  );
+}
+
+export default DetalheDePedidoCostumer;
---
+++ front-end/src/pages/MeusPedidosCostumer.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Orders from '../components/OrdersCustomer';
+import Header from '../components/Header';
+import { validLogin, setToken } from '../services/request';
+
+function MeusPedidosCostumer() {
+  const [roleUser, setUserRole] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // validação para token ao acessar a page
+    const getToken = JSON.parse(localStorage.getItem('user'));
+    const { token, role } = getToken;
+    setUserRole(role);
+    const requestValid = async () => {
+      try {
+        setToken(token);
+        const validToken = await validLogin('/login/validate');
+        if (!validToken) {
+          localStorage.setItem('user', '');
+          navigate('/login');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    requestValid();
+  }, [navigate]);
+
+  return (
+    <div>
+      { roleUser ? (
+        <div>
+          <Header />
+          <Orders role={ roleUser } />
+        </div>
+      ) : <p>Carregando...</p> }
+    </div>
+  );
+}
+
+export default MeusPedidosCostumer;
